bdbdd69f40117165d5d324e2cf1245107f4eca96
Ameng <amengsec@qq.com>
2022-02-03 14:36:15 +0800

    update

---
+++ pkg/level/level.go
@@ -0,0 +1,22 @@
+package level
+
+/*
+创建人员：云深不知处
+创建时间：2022/2/3
+程序功能：日志等级
+*/
+
+type Thelevel int
+
+const (
+	Fatal Thelevel = iota
+	Info
+	Warning
+	Error
+	Debug
+	Verbose
+)
+
+func (L Thelevel) Level() string {
+	return [...]string{"FATA", "INFO", "WARN", "ERROR", "DEBG", "VEBO"}[L]
+}
